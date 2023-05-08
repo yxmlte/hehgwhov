0ebf9b3fecc3667076eab585bb7e61cfe951887b
yxmlte <132931665+yxmlte@users.noreply.github.com>
2023-05-09 02:09:29 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-s = document.createElement('p');
+const s = document.createElement('p');
 s.innerText = "loaded";
 s.style = "position:fixed;top:0px;left:0px;background-color:red;"
 document.body.appendChild(s);
@@ -6,4 +6,9 @@ document.body.appendChild(s);
 url = "https://yxmlte.github.io/hehgwhov/test.json";
 fetch(url)
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    const s = document.createElement('p');
+    s.innerText = data.message;
+    s.style = "position:fixed;top:10px;left:0px;background-color:blue;"
+    document.body.appendChild(s);
+  });
