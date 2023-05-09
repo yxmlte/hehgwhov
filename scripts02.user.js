2835a99161211ae7bea657db278cc84c2c0a5ab5
yxmlte <132931665+yxmlte@users.noreply.github.com>
2023-05-09 12:10:40 +0900

    Create scripts02.user.js

---
+++ scripts02.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Example JS Userscript
+// @description This a standard userscript with javascript code
+// @version 1.0
+// @updateURL https://yxmlte.github.io/hehgwhov/scripts02.user.js
+// ==/UserScript==
+
+alert("hello");
+console.log("I am an example userscript");
