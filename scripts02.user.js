d03c74ac2f0cdc113cbd59386fa3163ba2489aac
yxmlte <132931665+yxmlte@users.noreply.github.com>
2023-05-09 12:18:07 +0900

    Update scripts02.user.js

--- scripts02.user.js
+++ scripts02.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name        Example JS Userscript
+// @name        Example JS Userscript 2
 // @description This a standard userscript with javascript code
-// @match       https://*
-// @version 1.0
+// @match       https://*/*
+// @version 1.1
 // @updateURL https://yxmlte.github.io/hehgwhov/scripts02.user.js
 // ==/UserScript==
 
